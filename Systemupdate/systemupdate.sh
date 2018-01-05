412264488c32eb28d130dbb560351d92faad09c5
Hermonella <tovare13@gmail.com>
2018-01-06 00:46:25 +0100

    Fixed typo

--- Systemupdate/systemupdate.sh
+++ Systemupdate/systemupdate.sh
@@ -10,7 +10,7 @@ apt upgrade
 apt dist-upgrade
 echo "Done"
 
-read -p "Restart Comuper? " -n 1 -r
+read -p "Restart Computer? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
